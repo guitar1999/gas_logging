bf54883660893baf2670cde71b6eff334b8ba025
Jesse Bishop <jessebishop@yahoo.com>
2014-06-26 09:05:57 -0400

    increasing functionality

--- generate_summary_dehumidifier_daily.py
+++ generate_summary_dehumidifier_daily.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import argparse, ConfigParser, datetime, psycopg2
 from tweet import *
 
@@ -17,10 +19,19 @@ dbuser = config.get('pidb', 'DBUSER')
 db = psycopg2.connect(host=dbhost, database=dbname, user=dbuser)
 cursor = db.cursor()
 
+# Current hack query to get dehumidifier runtime. This will need to be adjusted once the system
+# is plumbed to the air handler or the heat is running at the same time.
 query = """SELECT ROUND(SUM(tdiff) / 60 / 60, 2) AS runtime FROM electricity_measurements WHERE watts_ch3 > 490 AND measurement_time::date = (CURRENT_TIMESTAMP - interval '1 day')::date;"""
 cursor.execute(query)
 data = cursor.fetchall()
 runtime = data[0][0]
 
-status = """The dehumidifier ran for {0} hours yesterday.""".format(runtime)
+# Get the weather info
+query = """SELECT mean_dewpoint, mean_temperature FROM weather_daily_mean_data WHERE date = (CURRENT_TIMESTAMP - interval '1 day')::date;"""
+cursor.execute(query)
+data = cursor.fetchall()
+dew, t = data[0]
+
+# Tweet!
+status = """The dehumidifier ran for {0} hours yesterday. The average temperature was {1} and the average dewpoint was {2}.""".format(runtime, t, dew)
 tweet(status)
