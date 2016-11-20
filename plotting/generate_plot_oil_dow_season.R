9f7816f87500097742fcd1e239873cb431b89a25
Jesse Bishop <guitar1999@aol.com>
2016-11-20 10:05:29 -0500

    seasonal plot with stats

---
+++ plotting/generate_plot_oil_dow_season.R
@@ -0,0 +1,22 @@
+if (! 'package:RPostgreSQL' %in% search()) {
+    library(RPostgreSQL)
+    source('/home/jessebishop/.rconfig.R')
+}
+
+source('/usr/local/electricity_logging/plotting/barplot.R')
+
+query <- "WITH dates AS (SELECT generate_series::DATE AS d FROM generate_series(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP - interval '6 days', '-1 day')) SELECT u.day_of_week AS label, u.dow, u.btu, s.btu_avg, u.complete FROM dates o INNER JOIN oil.oil_usage_dow u ON DATE_PART('dow', o.d)=u.dow INNER JOIN weather_data.meteorological_season m ON DATE_PART('doy', o.d)=m.doy LEFT JOIN oil_statistics.oil_statistics_dow_season s ON u.dow=s.dow AND m.season=s.season ORDER BY o.d;"
+res <- dbGetQuery(con, query)
+
+res$btu <- res$btu / 1000
+
+fname <- '/var/www/electricity/ng_dow.png'
+title <- "Boiler BTUs Used in the Last Week"
+label.x <- "Day"
+label.y <- "Thousand BTU"
+
+png(filename=fname, width=1024, height=400, units='px', pointsize=12, bg='white')
+barplot(res$btu, names.arg=res$label, col='orange', las=1, main=title, ylab=label.y)
+dev.off()
+
+system(paste("scp", fname, "web309.webfaction.com:/home/jessebishop/webapps/htdocs/home/frompi/electricity/", sep=' '),ignore.stdout=TRUE,ignore.stderr=TRUE)
