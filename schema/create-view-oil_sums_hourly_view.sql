9598ce4c11d743ad83f032f0dbdc253169ed43f7
Jesse Bishop <jessebishop@yahoo.com>
2017-10-10 15:51:37 -0400

    fix missing current hour

--- schema/create-view-oil_sums_hourly_view.sql
+++ schema/create-view-oil_sums_hourly_view.sql
@@ -4,6 +4,19 @@ CREATE OR REPLACE VIEW oil_statistics.oil_sums_hourly_view AS (
         hour,
         runtime
     FROM
-        oil_statistics.oil_sums_hourly
+        (SELECT
+            sum_date,
+            hour,
+            runtime
+        FROM
+            oil_statistics.oil_sums_hourly
+        UNION
+        SELECT
+            CURRENT_TIMESTAMP::DATE AS sum_date,
+            DATE_PART('HOUR', CURRENT_TIMESTAMP)::INTEGER AS hour,
+            total_boiler_runtime AS runtime
+        FROM
+            boiler_summary(DATE_TRUNC('HOUR', CURRENT_TIMESTAMP)::TIMESTAMP, CURRENT_TIMESTAMP::TIMESTAMP)
+        ) AS x
     ORDER BY 1, 2
 );
