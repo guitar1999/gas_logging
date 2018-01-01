b330b00acb19bfdbf31e5b3067a44bdca004c168
Jesse Bishop <jessebishop@yahoo.com>
2018-01-01 16:54:08 -0500

    stats fix

--- schema/create-view-oil_statistics_monthly_view.sql
+++ schema/create-view-oil_statistics_monthly_view.sql
@@ -1,8 +1,8 @@
-CREATE VIEW oil_statistics.oil_statistics_monthly_view AS (
+CREATE OR REPLACE VIEW oil_statistics.oil_statistics_monthly_view AS (
     SELECT
         month,
-        avg(runtime) AS runtime_avg,
-        count(*) AS count
+        avg(runtime) FILTER (WHERE NOT DATE_PART('YEAR', CURRENT_TIMESTAMP) = year OR NOT DATE_PART('MONTH', CURRENT_TIMESTAMP) = month) AS runtime_avg,
+        count(*) FILTER (WHERE NOT DATE_PART('YEAR', CURRENT_TIMESTAMP) = year OR NOT DATE_PART('MONTH', CURRENT_TIMESTAMP) = month) AS count
     FROM
         oil_statistics.oil_sums_monthly_view
     -- WHERE
