0e3d4ae79a8555cba83f951b8a276261c1a9f1ec
Jesse Bishop <jessebishop@yahoo.com>
2020-09-03 12:13:42 -0400

    boiler sum trigger function

---
+++ schema/create-function-boiler_hourly_sum.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION boiler_hourly_sum() RETURNS TRIGGER AS 
+$$
+    DECLARE
+        old_hour INTEGER;
+    BEGIN
+        SELECT DATE_PART('HOUR', (SELECT MAX(measurement_time) FROM electricity_iotawatt.electricity_measurements WHERE NOT emid = NEW.emid)) INTO old_hour;
+        IF old_hour != DATE_PART('HOUR', NEW.measurement_time) THEN
+            INSERT INTO oil_statistics.oil_sums_hourly (sum_date, hour, btu, runtime)
+            SELECT (DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL)::DATE AS sum_date,
+            DATE_PART('HOUR', DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL) AS hour,
+            bs.btu,
+            bs.total_boiler_runtime
+            FROM boiler_summary( 
+                (DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL)::TIMESTAMP, (DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL)::TIMESTAMP + '00:59:59'::INTERVAL ) AS bs;
+            RETURN NEW;
+        ELSE
+            RETURN NEW;
+        END IF;
+    END;
+$$
+LANGUAGE PLPGSQL VOLATILE;
+
+
+DROP TRIGGER IF EXISTS boiler_hourly_sum ON electricity_iotawatt.electricity_measurements;
+CREATE TRIGGER boiler_hourly_sum
+    AFTER INSERT ON electricity_iotawatt.electricity_measurements FOR EACH ROW EXECUTE PROCEDURE boiler_hourly_sum();
