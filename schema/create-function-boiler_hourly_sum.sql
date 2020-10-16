9280f1a15a771006ba3c1c1a4b8f4fc6231fe67b
Jesse Bishop <jessebishop@yahoo.com>
2020-10-15 21:34:03 -0400

    function tweak

--- schema/create-function-boiler_hourly_sum.sql
+++ schema/create-function-boiler_hourly_sum.sql
@@ -8,8 +8,8 @@ $$
             INSERT INTO oil_statistics.oil_sums_hourly (sum_date, hour, btu, runtime)
             SELECT (DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL)::DATE AS sum_date,
             DATE_PART('HOUR', DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL) AS hour,
-            bs.btu,
-            bs.total_boiler_runtime
+            COALESCE(bs.btu, 0),
+            COALESCE(bs.total_boiler_runtime, 0)
             FROM boiler_summary( 
                 (DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL)::TIMESTAMP, (DATE_TRUNC('HOUR', NEW.measurement_time) - '1 HOUR'::INTERVAL)::TIMESTAMP + '00:59:59'::INTERVAL ) AS bs;
             RETURN NEW;
